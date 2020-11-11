2656a7aa0eac5b1ab37ca70554b2645e5be47c58
simoneinverardi <71282425+simoneinverardi@users.noreply.github.com>
2020-11-11 11:49:10 +0100

    p5

---
+++ public/sketch.js
@@ -0,0 +1,16 @@
+let socket = io();
+
+function preload(){
+  // put preload code here
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight)
+  // put setup code here
+  background(200);
+
+}
+
+function draw() {
+  // put drawing code here
+}
--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 console.log("node is running");
+
+let express = require("express");
+
+let socket = require("socket.io");
+
+let app = express();
+
+let port = 3000;
+
+let server = app.listen(port);
+
+app.use(express.static("public"));
+
+let io = socket(server);
+
+io.on("connection", newConnection);
+
+function newConnection(socket) {
+  console.log("new connection: " + socket.client.id);
+}
