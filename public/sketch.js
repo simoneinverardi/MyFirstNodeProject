51ee08eaf9d6b37e2309a9a4532b045b64a85b52
simoneinverardi <71282425+simoneinverardi@users.noreply.github.com>
2020-11-11 13:22:37 +0100

    alomst finish

--- public/sketch.js
+++ public/sketch.js
@@ -1,15 +1,24 @@
 let socket = io();
+let myColor = "white";
 
 socket .on("connect", newConnection);
 socket.on("mouseBroadcast", drawOtherMouse);
+socket.on("color", setColor);
+
+function setColor(assignedColor) {
+  myColor = assignedColor;
+
+}
 
 function newConnection(){
   console.log("your id: " + socket.id);
 }
 
 function drawOtherMouse(data) {
-  fill("yellow");
+  push();
+  fill(data.color);
   ellipse(data.x,data.y,10);
+  pop();
 }
 
 function preload(){
@@ -29,12 +38,15 @@ function draw() {
 }
 
 function mouseMoved(){
-  fill("wthite")
+  push();
+  fill(myColor);
   ellipse(mouseX,mouseY,20);
+  pop();
   //create the message
   let message = {
     x: mouseX,
     y: mouseY,
+    color: myColor,
   };
   //send to the server
   socket.emit("mouse", message);
--- server.js
+++ server.js
@@ -19,10 +19,23 @@ io.on("connection", newConnection);
 function newConnection(socket) {
   console.log("new connection: " + socket.client.id);
 
+  let clientColor = getRandomColor;
+
+  socket.emit("color", clientColor);
+
   socket.on("mouse", mouseMessage);
 
   function mouseMessage(dataReceived){
     console.log(socket.client.id, dataReceived);
     socket.broadcast.emit("mouseBroadcast", dataReceived);
   }
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
