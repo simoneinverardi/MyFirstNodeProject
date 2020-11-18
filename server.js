66242a73da790a52927b0307f03d4a6c859023f1
simoneinverardi <71282425+simoneinverardi@users.noreply.github.com>
2020-11-18 11:15:08 +0100

    heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ let express = require("express");
 let socket = require("socket.io");
 
 let app = express();
-let port = 3000;
+let port = process.env.PORT || 3000;
 
 let server = app.listen(port);
 
